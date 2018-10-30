65d84c0aea0e2155e8b7b933b4a852a2418d2a09
davidvs <davidvargas.d45@gmail.com>
2018-10-30 16:37:07 -0500

    creating Media component

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Media from './src/playlist/components/media'
+
+// react sive para crear componentes
+// reactDOM para renderizar y ponerlos en algún lugar
+
+const app = document.getElementById('app')
+/* const holaMundo = <h1>Hola Mundo!</h1> */
+
+/* ReactDOM.render(qué renderizo, dónde lo hago)*/
+ReactDOM.render(<Media />, app)
