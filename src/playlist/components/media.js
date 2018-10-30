2c8f0a92f69f0777c07d308a09e2dc737810525f
davidvs <davidvargas.d45@gmail.com>
2018-10-30 16:33:18 -0500

    inicializando

---
+++ src/playlist/components/media.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+// Componente funcional
+// Componente puro
+// Componente de estado
+
+class Media extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <img 
+                        src="" 
+                        alt=""
+                        width={260}
+                        height={160} 
+                    />
+                    <h3>¿Por qué aprender React?</h3>
+                    <p>Leonidas Esteban</p>
+                </div>
+                <p>Hola Mundo</p>
+            </div>
+        )
+    }
+}
+
+export default Media
